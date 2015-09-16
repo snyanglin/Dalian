ca0264d549073e6ddc192e8145e670a97fcf5d2c
wu_ping <562368845@qq.com>
2015-09-16 11:13:54 +0800

    11

---
+++ syrk/sqjw/com/founder/sqjw/vo/CountMapVO.java
@@ -0,0 +1,41 @@
+package com.founder.sqjw.vo;
+
+public class CountMapVO {
+	private String id;
+
+	private String zbx;
+	private String zby;
+	private String title;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getZbx() {
+		return zbx;
+	}
+
+	public void setZbx(String zbx) {
+		this.zbx = zbx;
+	}
+
+	public String getZby() {
+		return zby;
+	}
+
+	public void setZby(String zby) {
+		this.zby = zby;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
