e411a38eb215140a9e33938dc379b808b6d8de2c
zhang.hai <zhang.hai@founder.com.cn>
2015-09-30 11:17:33 +0800

    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- syrk/zdrygl/com/founder/zdrygl/base/controller/ZdryJgdxqxjdjbController.java
+++ syrk/zdrygl/com/founder/zdrygl/base/controller/ZdryJgdxqxjdjbController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
