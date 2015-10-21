a0cd42e4856a35740792dc1c3a70d651c911addf
zhang.hai <zhang.hai@founder.com.cn>
2015-10-21 17:44:42 +0800

    添加规则引擎相关注释
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- syrk/drools/com/founder/drools/base/zdry/service/ZdryRuleService.java
+++ syrk/drools/com/founder/drools/base/zdry/service/ZdryRuleService.java
@@ -3,8 +3,6 @@ package com.founder.drools.base.zdry.service;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +10,41 @@ import com.founder.drools.base.zdry.model.ZdryGlMessageBean;
 import com.founder.drools.core.inteface.RuleService;
 import com.founder.zdrygl.base.dao.ZdryZdryZbDao;
 
+/**
+ * ****************************************************************************
+ * @Package:      [com.founder.drools.base.zdry.service.ZdryRuleService.java]  
+ * @ClassName:    [ZdryRuleService]   
+ * @Description:  [重点人员规则调用服务]   
+ * @Author:       [zhang.hai@founder.com.cn]  
+ * @CreateDate:   [2015年10月21日 下午5:17:58]   
+ * @UpdateUser:   [ZhangHai(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateDate:   [2015年10月21日 下午5:17:58，(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateRemark: [说明本次修改内容,(如多次修改保留历史记录，增加修改记录)]  
+ * @Version:      [v1.0]
+ */
 @Service
 public class ZdryRuleService {
+	
+	public static String LGSQ="LGSQ";//列管申请
+	public static String CGSQ="CGSQ";//撤管申请
+	public static String LGSPJG="LGSPJG";	
+	
 	@Autowired
 	private ZdryZdryZbDao zdryZdryZbDao;		
 	
 	@Autowired
 	private RuleService ruleService;
 	
+	/**
+	 * 
+	 * @Title: getTitleAndContents
+	 * @Description: TODO(重点人员消息规则获取)
+	 * @param @param xxlx 消息类型
+	 * @param @param paraMap
+	 * @param @return    设定文件
+	 * @return Map<String,Object>    返回类型
+	 * @throw
+	 */
 	public Map<String, Object> getTitleAndContents(String xxlx, Map<String, String> paraMap) {				
 		
 		//私有参数处理
--- syrk/drools/com/founder/drools/core/model/RuleConfig.java
+++ syrk/drools/com/founder/drools/core/model/RuleConfig.java
@@ -4,6 +4,18 @@ import org.drools.KnowledgeBase;
 
 import com.founder.zdrygl.core.utils.DroolsUtils;
 
+/**
+ * ****************************************************************************
+ * @Package:      [com.founder.drools.core.model.RuleConfig.java]  
+ * @ClassName:    [RuleConfig]   
+ * @Description:  [规则引擎对象]   
+ * @Author:       [zhang.hai@founder.com.cn]  
+ * @CreateDate:   [2015年10月21日 下午5:14:50]   
+ * @UpdateUser:   [ZhangHai(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateDate:   [2015年10月21日 下午5:14:50，(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateRemark: [说明本次修改内容,(如多次修改保留历史记录，增加修改记录)]  
+ * @Version:      [v1.0]
+ */
 public class RuleConfig {
 	//规则服务URL	
 	private String url = null;
@@ -18,6 +30,14 @@ public class RuleConfig {
 		
 	}
 	
+	/**
+	 * 
+	 * <p>Title: </p>
+	 * <p>Description: </p>
+	 * @param url 规则引擎URL
+	 * @param userName 规则服务器用户名
+	 * @param userPassword 规则服务器密码
+	 */
 	public RuleConfig(String url,String userName,String userPassword){
 		this.url=url;
 		this.userName=userName;
@@ -48,6 +68,14 @@ public class RuleConfig {
 		this.userPassword = userPassword;
 	}
 
+	/**
+	 * 
+	 * @Title: getKbase
+	 * @Description: TODO(获取KnowledgeBase，第一次调用的时候初始化)
+	 * @param @return    设定文件
+	 * @return KnowledgeBase    返回类型
+	 * @throw
+	 */
 	public KnowledgeBase getKbase() {
 		if(kbase == null){
 			kbase = DroolsUtils.buildKnowledgeBaseByUrl(url, userName, userPassword);	
