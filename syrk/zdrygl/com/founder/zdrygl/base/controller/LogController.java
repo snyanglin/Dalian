d509b30a6d001c2e782f9c8e539b30dee814ecac
zhang.hai <zhang.hai@founder.com.cn>
2015-10-26 10:48:39 +0800

    交易统计接口修改
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- syrk/zdrygl/com/founder/zdrygl/base/controller/LogController.java
+++ syrk/zdrygl/com/founder/zdrygl/base/controller/LogController.java
@@ -1,5 +1,9 @@
 package com.founder.zdrygl.base.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.founder.framework.annotation.RestfulAnnotation;
 import com.founder.framework.base.controller.BaseController;
 import com.founder.framework.operationLog.service.OperationLogService;
+import com.founder.framework.utils.DateTimeHelper;
 
 /**
  * ****************************************************************************
@@ -43,7 +48,38 @@ public class LogController extends BaseController {
 	@RestfulAnnotation(serverId="3")
 	@RequestMapping(value = "/countTrs", method = {RequestMethod.GET,RequestMethod.POST})
 	public @ResponseBody List countTrs(){		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		Date date=new Date();
+		String endDate=sdf.format(date);
+		String startDate=sdf.format(DateTimeHelper.addSeconds(date,-60));
+		List list=new LinkedList();
+		//1、按操作成功失败统计：OPERATE_RESULT  0=失败  1=成功
+		List<String> resultList=new ArrayList<String>(2);
+		resultList.add("0");
+		resultList.add("1");
+		operationLogService.countByResult(list,startDate,endDate,resultList);
+		//2、按操作类型统计：OPERATE_TYPE   1=查询 2=新增  3=修改 4=注销
+		List<String> typeList=new ArrayList<String>(4);
+		typeList.add("1");
+		typeList.add("2");
+		typeList.add("3");
+		typeList.add("4");
+		operationLogService.countByType(list,startDate,endDate,typeList);
+		//3、按模块名称统计：MODNAME
+		List<String> modList=new ArrayList<String>(4);
+		modList.add("重点人员管理");
+		modList.add("重点人员查询服务");
+		modList.add("重点人员补充信息－评审鉴定");
+		modList.add("重点人员补充信息－违法犯罪记录");
+		operationLogService.countByMod(list,startDate,endDate,modList,4);
+		//4、按单位统计：ORGANIZATION
+		List<String> orgList=new ArrayList<String>(4);
+		orgList.add("解放责任区1");
+		orgList.add("白山路派出所");
+		orgList.add("沙河口分局");
+		orgList.add("解放责任区2");
+		operationLogService.countByOrg(list,startDate,endDate,orgList,4);
 		
-		return operationLogService.countTrs();
+		return list;			
 	}		
 }
