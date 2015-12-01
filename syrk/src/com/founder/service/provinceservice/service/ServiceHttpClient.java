348d31bec89d520c6dc8b306ad07f78a43477a39
weiwen <pepsi2009@vip.qq.com>
2015-12-01 17:14:30 +0800

    baseframework包调整
    Signed-off-by: weiwen <pepsi2009@vip.qq.com>

--- syrk/src/com/founder/service/provinceservice/service/ServiceHttpClient.java
+++ syrk/src/com/founder/service/provinceservice/service/ServiceHttpClient.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import net.sf.json.JSONObject;
 
 import com.founder.framework.config.SystemConfig;
-import com.founder.framework.httpService.BaseHttpClient;
+import com.founder.framework.httpService.CCSHttpClient;
 import com.founder.framework.httpService.HttpClientResultBean;
 import com.founder.framework.utils.StringUtils;
 import com.founder.service.provinceservice.bean.ServiceLdyBean;
@@ -29,7 +29,7 @@ import com.founder.service.provinceservice.bean.ServiceZtryBean;
  * @UpdateRemark: [说明本次修改内容,(如多次修改保留历史记录，增加修改记录)]
  * @Version: [v1.0]
  */
-public class ServiceHttpClient extends BaseHttpClient {
+public class ServiceHttpClient extends CCSHttpClient {
 
 	private static Map<String, String> map = new HashMap<String, String>();
 	static {
