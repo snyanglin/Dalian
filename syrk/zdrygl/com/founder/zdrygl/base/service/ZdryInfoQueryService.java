97a1ed738d00d38ec279e56cdb63a9b991e0455b
stdeay <261003737@qq.com>
2015-12-03 16:46:28 +0800

    新增加了queryForObject()方法，替代原来zdry中的同名方法

--- syrk/zdrygl/com/founder/zdrygl/base/service/ZdryInfoQueryService.java
+++ syrk/zdrygl/com/founder/zdrygl/base/service/ZdryInfoQueryService.java
@@ -79,6 +79,10 @@ public class ZdryInfoQueryService extends ZdryQueryService {
 		return zdryZdryZbDao.queryById(zdryzbId);
 	}
 	
+	@Override
+	public Integer queryForCount(String syrkId) {
+		return zdryZdryZbDao.queryCount(syrkId);
+	}
 	/**
 	 * 
 	 * @Title: getChildList
