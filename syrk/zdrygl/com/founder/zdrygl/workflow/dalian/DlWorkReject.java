379aa1d10bb93ec2dba82088f81622b080328c9e
iron <270487829@qq.com>
2016-01-05 11:09:28 +0800

    1. 自动创建、更新Activiti 数据库 2. avoid orgCode is null
    Change-Id: I768b2524bd983f1e8b367cdfe3a25ff17996341d
    Signed-off-by: iron <270487829@qq.com>

--- syrk/zdrygl/com/founder/zdrygl/workflow/dalian/DlWorkReject.java
+++ syrk/zdrygl/com/founder/zdrygl/workflow/dalian/DlWorkReject.java
@@ -61,10 +61,12 @@ public class DlWorkReject extends WorkflowDelegate {
 		ZdryZbUtil.setXtZxyy(zdrylbdx, "审批未通过");
 		if(sqlxdm.equals("01")){//列管
 			Boolean isSzObj 	= (Boolean) variables.get("isSz");
-			
 			String isJdbm 	= (isSzObj==true?new Boolean(false):new Boolean(true)).toString();
 			sessionBean.getExtendMap().put("isJdbm", isJdbm);
-			sessionBean.getExtendMap().put("sqrOrgId", variables.get("sqrOrgId").toString());
+			Object orgCodeObj = variables.get("sqrOrgId");
+			if(orgCodeObj != null){
+				sessionBean.getExtendMap().put("sqrOrgId", orgCodeObj.toString());
+			}
 			zdryService.lgFail(sessionBean,entity);
 		}else if(sqlxdm.equals("02")){//撤管
 			Zdry subZdry = ZdryZbUtil.getZdrylbdx( (Zdrycx) entity.getZdrycx());
